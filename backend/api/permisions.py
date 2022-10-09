513008978cee6d6b15c915398eb569116fca0f30
Олег Маркин <amstloleg@yandex.ru>
2022-10-09 16:33:29 +0700

    Done viewsets

--- backend/api/permisions.py
+++ backend/api/permisions.py
@@ -12,3 +12,16 @@ class AdminOrReadOnly(BasePermission):
                 or request.user.is_authenticated
                 and request.user.is_admin
         )
+
+
+class AuthorStaffOrReadOnly(IsAuthenticatedOrReadOnly):
+    """
+    Разрешение на изменение только для служебного персонала и автора.
+    Остальным только чтение объекта.
+    """
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in ('GET',)
+            or (request.user == obj.author)
+            or request.user.is_staff
+        )
