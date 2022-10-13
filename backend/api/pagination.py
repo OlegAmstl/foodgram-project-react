aa8c410df321cc6aca4665be50be8adb94427f83
Олег Маркин <amstloleg@yandex.ru>
2022-10-13 07:52:33 +0700

    fix dockerfile

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -2,6 +2,8 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class FoodgramPaginator(PageNumberPagination):
+    '''Паджинатор для вывода рецептов.'''
+
     page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 100
