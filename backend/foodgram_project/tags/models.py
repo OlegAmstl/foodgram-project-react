1956755bac0b4421fe5cea67a3c540947900f396
Олег Маркин <amstloleg@yandex.ru>
2022-10-24 20:19:40 +0700

    fix tags

--- backend/foodgram_project/tags/models.py
+++ backend/foodgram_project/tags/models.py
@@ -27,6 +27,20 @@ class Tag(models.Model):
         verbose_name = 'Тег'
         verbose_name_plural = 'Теги'
         ordering = ('name',)
+        constraints = (
+            models.CheckConstraint(
+                check=models.Q(name__length__gt=0),
+                name='\n%(app_label)s_%(class)s_name is empty\n',
+            ),
+            models.CheckConstraint(
+                check=models.Q(color__length__gt=0),
+                name='\n%(app_label)s_%(class)s_color is empty\n',
+            ),
+            models.CheckConstraint(
+                check=models.Q(slug__length__gt=0),
+                name='\n%(app_label)s_%(class)s_slug is empty\n',
+            ),
+        )
 
     def __str__(self):
         return f'Tag: {self.slug}-{self.color}'
